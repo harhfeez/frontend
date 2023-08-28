03b40e88b1b3395598b16e6f3550c41c25cd4864
@Peter-Temy <peteroyeleke029@gmail.com>
2023-08-28 17:07:14 +0100

    complete

---
+++ template/index.js
@@ -0,0 +1,13 @@
+function closeMenu(){
+    document.getElementById("dropdown_content").style.display="none";
+    document.getElementById("dropdown_togle").style.display="block";
+    document.getElementById("dropdown_togleX").style.display="none";
+}
+
+function openMenu(){
+    document.getElementById("dropdown_content").style.display="block";
+    document.getElementById("dropdown_togle").style.display="none"
+    document.getElementById("dropdown_togleX").style.display="block";
+}
+
+
